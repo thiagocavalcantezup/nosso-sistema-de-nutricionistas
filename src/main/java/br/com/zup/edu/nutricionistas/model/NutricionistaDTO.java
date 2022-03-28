ddbfbcb234bf42d291b45359eb4670fd2e5936e7
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 18:54:39 -0300

    Fix DTO conversion method

--- src/main/java/br/com/zup/edu/nutricionistas/model/NutricionistaDTO.java
+++ src/main/java/br/com/zup/edu/nutricionistas/model/NutricionistaDTO.java
@@ -47,7 +47,7 @@ public class NutricionistaDTO {
     }
 
     public Nutricionista paraNutricionista() {
-        String novoCrn = cpf.replaceAll("[^0-9A-Z/]", "");
+        String novoCrn = crn.replaceAll("[^0-9A-Z/\\-, ]", "");
         String novoCpf = cpf.replaceAll("[^0-9]", "");
 
         return new Nutricionista(novoCrn, nome, novoCpf, dataNascimento, email);
