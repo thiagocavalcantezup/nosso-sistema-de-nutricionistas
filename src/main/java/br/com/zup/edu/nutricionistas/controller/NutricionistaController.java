b3484fe8c16e40baf7cb12049a45fba7142af6e2
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 18:54:24 -0300

    Add nutricionista creation controller method

--- src/main/java/br/com/zup/edu/nutricionistas/controller/NutricionistaController.java
+++ src/main/java/br/com/zup/edu/nutricionistas/controller/NutricionistaController.java
@@ -1,8 +1,18 @@
 package br.com.zup.edu.nutricionistas.controller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.edu.nutricionistas.model.Nutricionista;
+import br.com.zup.edu.nutricionistas.model.NutricionistaDTO;
 import br.com.zup.edu.nutricionistas.repository.NutricionistaRepository;
 
 @RestController
@@ -15,6 +25,20 @@ public class NutricionistaController {
 
     public NutricionistaController(NutricionistaRepository nutricionistaRepository) {
         this.nutricionistaRepository = nutricionistaRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid NutricionistaDTO nutricionistaDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Nutricionista nutricionista = nutricionistaRepository.save(
+            nutricionistaDTO.paraNutricionista()
+        );
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(nutricionista.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
