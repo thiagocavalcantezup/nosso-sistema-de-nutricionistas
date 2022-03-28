1227eac1853b3a8c1c58e5b8edacf75d018d7b15
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 16:58:31 -0300

    Format repository

--- src/main/java/br/com/zup/edu/nutricionistas/repository/NutricionistaRepository.java
+++ src/main/java/br/com/zup/edu/nutricionistas/repository/NutricionistaRepository.java
@@ -1,7 +1,9 @@
 package br.com.zup.edu.nutricionistas.repository;
 
-import br.com.zup.edu.nutricionistas.model.Nutricionista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NutricionistaRepository extends JpaRepository<Nutricionista,Long> {
+import br.com.zup.edu.nutricionistas.model.Nutricionista;
+
+public interface NutricionistaRepository extends JpaRepository<Nutricionista, Long> {
+
 }
