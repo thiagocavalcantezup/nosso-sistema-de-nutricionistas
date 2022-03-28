ec65e416e3e888a5a389baa0d0536116a9dac819
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 16:56:32 -0300

    Update Nutricionista DTO class
    Changes:
    - Add missing validations
    - Change attribute order and names to match entity

---
+++ src/main/java/br/com/zup/edu/nutricionistas/model/NutricionistaDTO.java
@@ -0,0 +1,69 @@
+package br.com.zup.edu.nutricionistas.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+public class NutricionistaDTO {
+
+    @NotBlank
+    private String crn;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @CPF
+    private String cpf;
+
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @JsonProperty("data_nascimento")
+    @Past
+    private LocalDate dataNascimento;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    public NutricionistaDTO() {}
+
+    public NutricionistaDTO(@NotBlank String crn, @NotBlank String nome, @NotBlank @CPF String cpf,
+                            @NotNull @Past LocalDate dataNascimento,
+                            @NotBlank @Email String email) {
+        this.crn = crn;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+
+    public String getCrn() {
+        return crn;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
