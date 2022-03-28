ea4bb4c4612727c65abf6a59850030eb0b84ee76
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 16:59:42 -0300

    Add repository dependency to controller

--- src/main/java/br/com/zup/edu/nutricionistas/controller/NutricionistaController.java
+++ src/main/java/br/com/zup/edu/nutricionistas/controller/NutricionistaController.java
@@ -3,10 +3,18 @@ package br.com.zup.edu.nutricionistas.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.edu.nutricionistas.repository.NutricionistaRepository;
+
 @RestController
 @RequestMapping(NutricionistaController.BASE_URI)
 public class NutricionistaController {
 
     public final static String BASE_URI = "/nutricionistas";
+
+    private final NutricionistaRepository nutricionistaRepository;
+
+    public NutricionistaController(NutricionistaRepository nutricionistaRepository) {
+        this.nutricionistaRepository = nutricionistaRepository;
+    }
 
 }
