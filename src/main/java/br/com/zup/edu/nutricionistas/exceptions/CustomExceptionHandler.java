63b57ca66d7588d320e7c8b87da0ba399bfdfd36
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 10:46:39 -0300

    Add handler for MethodArgumentNotValidException
    This exception gets thrown when there's a validation error.

--- src/main/java/br/com/zup/edu/nutricionistas/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/zup/edu/nutricionistas/exceptions/CustomExceptionHandler.java
@@ -14,4 +14,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroPadronizado> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        HttpStatus badRequestStatus = HttpStatus.BAD_REQUEST;
+        Integer codigoHttp = badRequestStatus.value();
+        String mensagemHttp = badRequestStatus.getReasonPhrase();
+
+        List<String> mensagens = new ArrayList<>();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            String field = fieldError.getField();
+
+            if (field.equals("dataNascimento")) {
+                field = "data_nascimento";
+            }
+
+            mensagens.add(field + ": " + fieldError.getDefaultMessage());
+        }
+
+        String palavraErro = mensagens.size() == 1 ? "erro" : "erros";
+        String mensagemGeral = "Validação falhou com " + mensagens.size() + " " + palavraErro + ".";
+
+        ErroPadronizado erroPadronizado = new ErroPadronizado(
+            codigoHttp, mensagemHttp, mensagemGeral, mensagens
+        );
+
+        return ResponseEntity.badRequest().body(erroPadronizado);
+
+    }
+
 }
